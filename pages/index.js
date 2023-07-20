1641d26f4266f5fb1c800bd800f0ee05056b2b8d
WaYnEHWang <wayne0987880873@gmail.com>
2023-07-20 11:23:45 +0800

    sample

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+
+  const Button = () => {
+
+    const handleClick = () => {
+      fetch('http://192.168.1.126:8000/test')
+        .then(response => response.json())
+        .then(data => {
+          // Process the received data here
+          console.log(data);
+        })
+        .catch(error => {
+          // Handle any error that occurred
+          console.error(error);
+        });
+    };
+
+    return (
+      <button className={utilStyles.button} onClick={handleClick}>
+        Click me
+      </button>
+    );
+  }
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <Button/>
+    </Layout>
+  );
+}
