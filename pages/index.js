7dd4ff68ebee7e18afedcd5db44f6755a9add124
WaYnEHWang <wayne0987880873@gmail.com>
2023-07-21 13:39:39 +0800

    adding two buttons

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,27 @@ export default function Home() {
   const Button = () => {
 
     const handleClick = () => {
-      fetch('http://127.0.0.1:8000/test')
+      fetch('http://192.168.1.126:8000/smartPlug/on')
+        .then(response => response.json())
+        .then(data => {
+          // Process the received data here
+          console.log(data);
+        })
+        .catch(error => {
+          // Handle any error that occurred
+          console.error(error);
+        });
+    };
+    return (
+      <button className={utilStyles.button} onClick={handleClick}>
+        Click me
+      </button>
+    );
+  }
+
+  const On = () => {
+    const handleClick = () => {
+      fetch('http://192.168.1.126:8000/smartPlug/on')
         .then(response => response.json())
         .then(data => {
           // Process the received data here
@@ -20,17 +40,42 @@ export default function Home() {
     };
 
     return (
-      <button className={utilStyles.button} onClick={handleClick}>
-        Click me
+      <button className={utilStyles.on} onClick={handleClick}>
+        電燈打開
       </button>
     );
   }
+
+  const Off = () => {
+    const handleClick = () => {
+      fetch('http://192.168.1.126:8000/smartPlug/off')
+        .then(response => response.json())
+        .then(data => {
+          // Process the received data here
+          console.log(data);
+        })
+        .catch(error => {
+          // Handle any error that occurred
+          console.error(error);
+        });
+    };
+
+    return (
+      <button className={utilStyles.off} onClick={handleClick}>
+        電燈關閉
+      </button>
+    );
+  }
+
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <Button/>
+      <On />
+      <Off/>
     </Layout>
   );
 }
